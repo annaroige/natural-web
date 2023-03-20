fceb665339421b62ac4abdf2c381a85720f00a76
Anna Roigé Nogués <Anna@macbookNNAROIGE.lan>
2023-03-20 10:48:12 +0100

    Initial commit

---
+++ src/main/java/com/lasalle/naturalweb/NaturalWebApplication.java
@@ -0,0 +1,13 @@
+package com.lasalle.naturalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaturalWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lasalle/naturalweb/NaturalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasalle.naturalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
